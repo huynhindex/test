9865c0502065cee024f5dcbe6296ae6dd9c18d33
index linux <huynhindex@gmail.com>
2025-07-12 12:48:21 +0000

    Allow printting more than one error message

--- all_checks.py
+++ all_checks.py
@@ -24,14 +24,17 @@ def check_root_full():
 
 
 def main():
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    if check_root_full():
-        print("Root partition full.")
-        sys.exit(1)
-
-    print("Everything ok.")
+    checks=[
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root partition full"),
+    ]
+    everything_ok=True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok=False
+    if everything_ok:
+        print("Everything ok.")
     sys.exit(0)
 
 main()
