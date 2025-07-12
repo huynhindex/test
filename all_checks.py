b6b0fcc7442d267319001d810f76f49194d165ae
index <indexhuynh@gmail.com>
2025-07-12 22:01:04 +0700

    edit for check network

--- all_checks.py
+++ all_checks.py
@@ -36,6 +36,7 @@ def main():
     checks=[
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
+        (check_no_network, "No working network."),
     ]
     everything_ok=True
     for check, msg in checks:
